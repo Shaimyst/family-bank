87faace226f7c97ccbb301325c31286b2fe9d36a
Jessica Sadler <jessica.sadler@scrive.com>
2025-01-26 19:10:14 +0100

    add fields for transaction input, start implementing transaction logic

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("Family Bank")
 
 class ChildAccount:
     def __init__(self, name: str, balance: float = 0.0):
@@ -20,10 +20,22 @@ class ChildAccount:
 willow_account = ChildAccount("Willow")
 penny_account = ChildAccount("Penny")
 
-st.write("Willow's balance: ", willow_account.get_balance())
 st.write("Penny's balance: ", penny_account.get_balance())
 
-st.write("Committing transaction to Penny's account of +100")
-penny_account.commit_transaction(100)
+st.header("Transaction")
+child = st.selectbox("Child: ", ["Willow", "Penny"])
+transaction_amount = st.number_input("Amount: ", step=1.0)
+transaction_description = st.text_input("Description: ")
+parent_name = st.selectbox("Parent: ", ["Dada", "Mamma"])
+parent_password = st.text_input("Password: ", type="password")
 
+if st.button("Commit transaction"):
+    if child == "Willow":
+        willow_account.commit_transaction(transaction_amount)
+    else:
+        penny_account.commit_transaction(transaction_amount)
+
+    st.write("Transaction successful")
+
+st.write("Willow's balance: ", willow_account.get_balance())
 st.write("Penny's balance: ", penny_account.get_balance())
